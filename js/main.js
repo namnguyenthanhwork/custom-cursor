311e966bf18c5267727cf3267312ba584b1c1899
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-03-07 21:17:39 +0700

    update
    - add loader when reset browser

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    // loader page when reload
+    $(window).on("load", function () {
+        $(".loader-wrapper").fadeOut("slow")
+    })
+
     $(window).scroll(function () {
         // scroll-up button show/hide script
         if (this.scrollY > 20) {
